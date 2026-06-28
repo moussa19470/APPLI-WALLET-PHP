2de5d9a1fd07e091a478565ae85ab6d006f5ae92
moussaba <moussakhady41@gmail.com>
2026-06-28 18:33:39 +0000

    feat: configuration de composer avec autoload files pour les fonctions namespacees

--- index.php
+++ index.php
@@ -5,20 +5,18 @@ use function App\Controller\faireUnDepot;
 use function App\Controller\faireUnRetrait;
 use function App\Controller\afficheListeTransaction;
 
-require_once 'controller.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 // Tableau numerique de wallets
 $wallets = [
     0 => ['client' => 'Moussa BA', 'telephone' => '775788482', 'codeSecret' => '1234', 'solde' => 0],
     1 => ['client' => 'Mr WANE', 'telephone' => '785763489', 'codeSecret' => '1010', 'solde' => 3000]
 ];
-
 $transactions = [
     0 => ['montant' => +5000, 'frais' => 0, 'indexClient' => 0],
     1 => ['montant' => -2000, 'frais' => 200, 'indexClient' => 1],
     2 => ['montant' => -3000, 'frais' => 200, 'indexClient' => 0]
 ];
-
 function afficheMenu(): void {
     echo "---Menu Distributeur---\n";
     echo "1. Creer Wallet \n";
@@ -28,15 +26,12 @@ function afficheMenu(): void {
     echo "0. Quitter \n";
     echo "-----------------------\n";
 }
-
 function lireChoix(): int {
     return (int) readline("Entrez votre choix : \n");
 }
-
 do {
     afficheMenu();
     $choix = lireChoix();
-
     switch ($choix) {
         case 1:
             ajouterWallet();
