d0c13520dc452da26eb1632b545da2d671dfd28e
moussaba <moussakhady41@gmail.com>
2026-06-28 12:16:26 +0000

    feat: ajout du menu interactif

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once 'controller.php';
+
+$wallets = [];
+$transactions = [];
+
+function afficheMenu(): void {
+    echo "---Menu Distributeur---\n";
+    echo "1. Creer Wallet \n";
+    echo "2. Faire Depot \n";
+    echo "3. Faire Retrait \n";
+    echo "4. Lister les Transactions \n";
+    echo "0. Quitter \n";
+    echo "-----------------------\n";
+}
+
+function lireChoix(): int {
+    return (int) readline("Entrez votre choix : \n");
+}
+
+do {
+    afficheMenu();
+    $choix = lireChoix();
+
+    switch ($choix) {
+        case 1:
+            echo "Fonctionnalite Creer Wallet a venir\n";
+            break;
+        case 2:
+            echo "Fonctionnalite Depot a venir\n";
+            break;
+        case 3:
+            echo "Fonctionnalite Retrait a venir\n";
+            break;
+        case 4:
+            echo "Fonctionnalite Transactions a venir\n";
+            break;
+        case 0:
+            break;
+        default:
+            echo "Choix invalide, veuillez reessayer\n";
+            break;
+    }
+} while ($choix != 0);
